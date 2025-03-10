c52f62b88ebd302285404dfb6469bcc352aec4a5
AchantaKrishnaPranav2 <ch23btech11005@iith.ac.in>
2025-03-10 20:11:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("BORON")
 st.sidebar.title("Properties")
 st.sidebar.link_button("Click here for the code","https://krishnapranav.streamlit.app/")
 
-st.radio("which element do you want to know about",["Boron","Carbon"]
+st.radio("which element do you want to know about",["Boron","Carbon"])
