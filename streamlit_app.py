a27072f1b128b3fc477a18d7c796ae471941de82
AchantaKrishnaPranav2 <ch23btech11005@iith.ac.in>
2025-03-10 19:54:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("BORON")
 st.sidebar.title("Properties")
-st.sidebar.link_button("Click here for the code", "krishnapranav.streamlit.app")
+st.sidebar.link_button("Click here for the code",  krishnapranav.streamlit.app)
 
 st.write("Hello world!")
