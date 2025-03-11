8eaf4511c8b7cac674ffb32603c18e9fa2f9cfc3
AchantaKrishnaPranav2 <ch23btech11005@iith.ac.in>
2025-03-11 16:01:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,10 +32,7 @@ if a == ":red[B]  :fire:" :
     height = 50,
     hide_index=True,
   )
-  col1, col2, col3 = st.columns(3)
-  col1.metric("Electron configuration", st.latex("2s^2"), "1.2 °F")
-  col2.metric("Wind", "9 mph", "-8%")
-  col3.metric("Humidity", "86%", "4%")
+ st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
   
 
 
