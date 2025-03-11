8be1a9f5e10f6186a985475437b7c023a0133174
AchantaKrishnaPranav2 <ch23btech11005@iith.ac.in>
2025-03-11 15:18:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.sidebar.link_button("Click here for the code","https://krishnapranav.streamli
 a = st.radio("Choose",["B","K"],captions = ["Boron","Potassium"] ,index = None)
 time.sleep(1)
 if a == "B":
-  st.text(":voilet[Boron] has the symbol B and atomic number 5")
+  st.text(":red[Boron] has the symbol B and atomic number 5")
   st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Boron_R105.jpg/330px-Boron_R105.jpg",caption = "boron (β-rhombohedral)",width = 200)
   time.sleep(2)
   st.text("Molecular weight: 10.811")
