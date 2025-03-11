effe1b165c3e46a86d3b5c6f617a309506f53fda
AchantaKrishnaPranav2 <ch23btech11005@iith.ac.in>
2025-03-11 12:12:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title("ELEMENTS")
 st.sidebar.title("Properties")
-st.header("Formula :red[B]")
-st.sidebar.header("Physical")
+st.header("Formula :red[B]:fire:",divider = True)
+st.sidebar.header("Physical",divider = True)
 st.sidebar.link_button("Click here for the code","https://krishnapranav.streamlit.app/")
 
 st.radio("which element do you want to know about",["Boron","Carbon"])
