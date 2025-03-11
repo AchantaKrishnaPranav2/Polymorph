a5d8fc99c8c1f16786d1f372042a84ff9818a860
AchantaKrishnaPranav2 <ch23btech11005@iith.ac.in>
2025-03-11 16:05:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,8 @@ if a == ":red[B]  :fire:" :
     height = 50,
     hide_index=True,
   )
-  st.metric(label="Electronic Configuration", value= st.latex("2s^2 2p^1"), delta="1.2 °F")
+  k = st.latex("2s^2 2p^1")
+  st.metric(label="Electronic Configuration", value= k , delta= None)
   
 
 
