eb9053b38868c21300b0ef7773ab61e8567b486a
AchantaKrishnaPranav2 <ch23btech11005@iith.ac.in>
2025-03-11 12:15:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ st.header("Formula :red[B]:fire:",divider = True)
 st.sidebar.header("Physical",divider = True)
 st.sidebar.link_button("Click here for the code","https://krishnapranav.streamlit.app/")
 
-st.radio("which element do you want to know about",["Boron","Carbon"])
+st.checkbox("Boron",value = True)
 st.button("submit")
