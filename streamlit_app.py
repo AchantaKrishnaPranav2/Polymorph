f0770eb12dd8cf04af8ea8e825097c25b318e3c9
AchantaKrishnaPranav2 <ch23btech11005@iith.ac.in>
2025-03-11 12:08:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("ELEMENTS")
 st.sidebar.title("Properties")
-st.header("Formula :orange[B]")
+st.header("Formula :magenta[B]")
 st.sidebar.header("Physical")
 st.sidebar.link_button("Click here for the code","https://krishnapranav.streamlit.app/")
 
