a3ff30b4a3b3b8f6166716c2480b5475c7aac2c1
AchantaKrishnaPranav2 <ch23btech11005@iith.ac.in>
2025-03-11 16:19:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,11 @@ if a == ":red[B]  :fire:" :
   )
   st.subheader("Electronic configuration :")
   st.latex("2s^2 2p^1 ")
-  
+
+  col1, col2, col3 = st.columns(3)
+  col1.metric("Group", "13")
+  col2.metric("Period", "2")
+  col3.metric("Block", "P-block")
    
   
   
